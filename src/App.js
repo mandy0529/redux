9fcd98f1d49a6258ea691ab429edddf220221195
Minji <321963741@naver.com>
2022-01-29 09:07:51 +0900

    createStore,getState

--- src/App.js
+++ src/App.js
@@ -1,17 +1,29 @@
-import React from "react";
+import React from 'react';
 // components
-import Navbar from "./components/Navbar";
-import CartContainer from "./components/CartContainer";
+import Navbar from './components/Navbar';
+import CartContainer from './components/CartContainer';
 // items
-import cartItems from "./cart-items";
+import cartItems from './cart-items';
 // redux stuff
+import {createStore} from 'redux';
+
+const reducer = (state, action) => {
+  console.log({state, action});
+  return state;
+};
+
+const initialState = {
+  count: 5,
+};
+
+const store = createStore(reducer, initialState);
 
 function App() {
   // cart setup
 
   return (
     <main>
-      <Navbar />
+      <Navbar cart={store.getState()} />
       <CartContainer cart={cartItems} />
     </main>
   );
