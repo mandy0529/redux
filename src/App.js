4b63465c31351883ea8bd9cf7f29f94bf3bc24cd
Minji <321963741@naver.com>
2022-01-29 09:32:02 +0900

    multiple state

--- src/App.js
+++ src/App.js
@@ -9,14 +9,28 @@ import {createStore} from 'redux';
 
 const reducer = (state, action) => {
   console.log({state, action});
+  if (action.type === 'DECREASE') {
+    return {...state, count: state.count - 1};
+  }
+  if (action.type === 'INCREASE') {
+    return {...state, count: state.count + 1};
+  }
+  if (action.type === 'CLEAR') {
+    return {...state, count: 0};
+  }
   return state;
 };
 
 const initialState = {
-  count: 5,
+  count: 0,
+  name: 'minji',
 };
 
 const store = createStore(reducer, initialState);
+
+store.dispatch({type: 'DECREASE'});
+store.dispatch({type: 'INCREASE'});
+store.dispatch({type: 'CLEAR'});
 
 function App() {
   // cart setup
