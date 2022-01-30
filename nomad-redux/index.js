51b99d3801857a0d3eef96be733b4953e5ad8e62
Minji <321963741@naver.com>
2022-01-30 10:29:59 +0900

    count done

--- nomad-redux/index.js
+++ nomad-redux/index.js
@@ -2,9 +2,32 @@ const plus = document.getElementById('plus');
 const minus = document.getElementById('minus');
 const number = document.querySelector('span');
 
-const reducer = (state = 0) => {
-  return state;
+const reducer = (state = 0, action) => {
+  switch (action.type) {
+    case 'PLUS':
+      return state + 1;
+
+    case 'MINUS':
+      return state - 1;
+
+    default:
+      break;
+  }
 };
 const store = Redux.createStore(reducer);
 
-console.log(store.getState(), 'store');
+const handleChange = () => {
+  number.textContent = store.getState();
+};
+
+const handlePlus = () => {
+  store.dispatch({type: 'PLUS'});
+};
+
+const handleMinus = () => {
+  store.dispatch({type: 'MINUS'});
+};
+
+plus.addEventListener('click', handlePlus);
+minus.addEventListener('click', handleMinus);
+store.subscribe(handleChange);
