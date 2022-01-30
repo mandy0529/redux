1d28f34f967141923c85006dad7db536b93f6124
Minji <321963741@naver.com>
2022-01-30 18:51:00 +0900

    fetchdata start

--- redux-clone/src/components/Product.js
+++ redux-clone/src/components/Product.js
@@ -1,7 +1,33 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import {useSelector} from 'react-redux';
+import axios from 'axios';
 
 function Product() {
-  return <div>product</div>;
+  const products = useSelector((state) => state);
+  console.log(products, 'products');
+
+  const fetchData = async () => {
+    const {data} = await axios('https://fakestoreapi.com/products');
+    console.log(data);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return (
+    <div className="four column wide">
+      <div className="ui link cards">
+        <div className="card">
+          <div className="image">
+            <div className="content">
+              <div className="header"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Product;
--- redux-clone/src/components/ProductList.js
+++ redux-clone/src/components/ProductList.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import Product from './Product';
 
 function ProductList() {
-  return <div>product list</div>;
+  return (
+    <div className="ui grid container">
+      <h1>product list</h1>
+      <Product />
+    </div>
+  );
 }
 
 export default ProductList;
